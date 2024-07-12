4bd4f8bf7827774a1773c3d4d8387f8a7d68446a
Damian <damianmarti@gmail.com>
2024-07-12 16:51:05 -0300

    Add Greetings item to Header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -20,6 +20,10 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
   {
+    label: "Greetings",
+    href: "/greetings",
+  },
+  {
     label: "Debug Contracts",
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
